6602d0c012809d818766fc0b976a16ed2b7f65e5
Mikhail Feoktistov <mikhail.feoktistov@betbright.com>
2019-06-19 11:33:11 +0100

    Fix php7.1 warning

--- src/libraries/CIUnit.php
+++ src/libraries/CIUnit.php
@@ -59,7 +59,8 @@ class CIUnit
 
     public static function &set_controller($controller = 'CI_Controller', $path = FALSE)
     {
-        $controller_name = array_pop(explode('/', $controller));
+        $parts = explode('/', $controller);
+        $controller_name = array_pop($parts);
         //echo "\nc name ".$controller_name;
         //is it the current controller?
         if ($controller_name == self::$current) {
